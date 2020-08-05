4b2d0e55579a0cabffacf07812165f42b28a2ac4
Yo! <yo@pop-os.localdomain>
2020-08-05 15:43:14 +0300

    Styles and some cross platform fixes

--- main.js
+++ main.js
@@ -2,16 +2,21 @@ const {app,BrowserWindow,Menu, ipcMain, Tray} =  require('electron');
 const path  = require('path');
 const url = require('url');
 
-var iconPath = path.join(__dirname,'./assets/icon.ico.ico');
+const platform  = process.platform;
 
+// choosing icon according to the paltform
+const logoPath = platform == 'linux' ? '/assets/icon.png' : platform == 'win32' ? '/assets/icon.ico.ico':'/assets/spsd';
+
+const  iconPath = path.join(__dirname,logoPath);
 let mainWindow;
 
 // Creating browser window
 function createWindow(width,height){
         mainWindow = new BrowserWindow({
         alwaysOnTop: true,
-        width:800,
+        width:500,
         height:600,
+        resizable:false,
         webPreferences:{
             nodeIntegration:true,
             backgroundThrottling:false
@@ -27,16 +32,22 @@ function createWindow(width,height){
 
     // Send into the tray on minimize
     mainWindow.on('minimize',(e)=>{
-        e.preventDefault();
-        appIcon = new Tray(iconPath);
-        appIcon.setContextMenu(contextMenu);
-
-        mainWindow.hide();
+        if(platform!="linux"){
+            e.preventDefault();
+            appIcon = new Tray(iconPath);
+            appIcon.setContextMenu(contextMenu);
+    
+            mainWindow.hide();
+        }
+        else{
+        }
 
     });
     // Destroy tray on restoring
     mainWindow.on('restore',()=>{
-        appIcon.destroy();
+        if(platform!='linux'){
+            appIcon.destroy();
+        }
         mainWindow.show();
     })
 
@@ -44,7 +55,7 @@ function createWindow(width,height){
         mainWindow = null;
     })
     mainWindow.on('show',()=>{
-        mainWindow.maximize();
+        // mainWindow.maximize();
     })
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
@@ -84,7 +95,6 @@ const mainMenuTemplate = [
     },
     {
         label:'Developer Tools',
-
         submenu:[
             {
                 label:'Toggle DevTools',
@@ -101,13 +111,8 @@ const mainMenuTemplate = [
     }
 ]
 
-
-ipcMain.on('hide:window',()=>{
-    mainWindow.hide();
-});
-
 ipcMain.on('maximize:window',()=>{
     mainWindow.show();
-    mainWindow.maximize();
+    // mainWindow.maximize();
 })
 
